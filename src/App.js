2535909e4190128b3c6bb4043091e6a813f0b077
SyedSami-UI <43094470+SyedSami-UI@users.noreply.github.com>
2019-03-08 09:53:18 +0530

    Formatted Code

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     return (
       <div className="App">
 		<SessionTimeOutSample></SessionTimeOutSample>
-        <RatingPanelSection></RatingPanelSection>
+        	<RatingPanelSection></RatingPanelSection>
       </div>
     );
   }
