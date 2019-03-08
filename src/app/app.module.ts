876f4dc16c9e32bb9bdd5eb6edd9b19b83bf46cc
saleem malik <saleemmalik.r@hcl.com>
2019-03-08 12:02:26 +0530

    src updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { QuickDatePickerModule } from './quick-date-picker/quick-date-picker.module';
 import { DatePipe } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ContextMenuModule } from 'src/app/context-menu/context-menu.module';
+import { ContextMenuModule } from './context-menu/context-menu.module';
 
 @NgModule({
   declarations: [
