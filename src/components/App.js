f148a6582e0cad5f401942afca683478bac5c1f1
Santosh Kumar Gopisetty <gopi.santo@gmail.com>
2019-03-10 22:30:29 +0100

    included server.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import IdleTimeout from './IdleTimeOut';
+import '../styles/App.css';
 
 const handleTimeOut = callback => {
     console.info('your session expired, hence restarting the timer again.');
@@ -14,8 +15,10 @@ const App = () => (
         showIdleTimeLeft
         showIdleTimeLeftBefore={4}
     >
-        <div>
-            This is a component which uses IdleTimeout. Pleaseait for 7 secs it will display log out popup. Also it will display the countdown timer when IdleTime left is 4 secs. Once if you click "Ok" on popup, it will restart the timer again for this example.
+        <div className="custom-component">
+            <p>
+                This is a component which uses IdleTimeout. Pleaseait for 7 secs it will display log out popup. Also it will display the countdown timer when IdleTime left is 4 secs. Once if you click "Ok" on popup, it will restart the timer again for this example.
+            </p>
         </div>
     </IdleTimeout>
 );
