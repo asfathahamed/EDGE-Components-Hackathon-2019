20236beba0aa59a11f96f24919bbfa0cd68b635d
Shivraj Chougule <36497430+ShivrajChougule@users.noreply.github.com>
2019-03-11 20:06:31 +0530

    Update app.component.spec.ts

--- date-spinner/src/app/app.component.spec.ts
+++ date-spinner/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { DateSpinnerComponent } from './date-spinner/date-spinner.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -9,7 +10,8 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        DateSpinnerComponent
       ],
     }).compileComponents();
   }));
@@ -18,18 +20,22 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+    fixture.detectChanges();
   });
 
-  it(`should have as title 'date-spinner'`, () => {
+  it('should get the selected date from date spinner', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('date-spinner');
+    const app = fixture.componentInstance;
+    app.getSelectedDate({year: 2019, month: '03', day: 11});
+   // expect(app.selectedDate).toEqual(2019 + ' / ' + '03' + ' / ' + 11);
+    fixture.detectChanges();
   });
 
-  it('should render title in a h1 tag', () => {
+
+ /*  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to date-spinner!');
-  });
+  }); */
 });
