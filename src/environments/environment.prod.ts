672368dce35043f66299bf5817a78a4ba77a5743
Ashutosh Pradhan <ashutosh.p@HCL.COM>
2019-03-12 01:49:36 +0530

    Adding some assets.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBGxklhGQfdXF7xfgsxWJmXxKAcnSfvgR8",
+    authDomain: "angular-commenting-app.firebaseapp.com",
+    databaseURL: "https://angular-commenting-app.firebaseio.com",
+    projectId: "angular-commenting-app",
+    storageBucket: "angular-commenting-app.appspot.com",
+    messagingSenderId: "1045335623395"
+  }
 };
